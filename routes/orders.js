c5cb4f2a293c6523ecf53d45bf72c5686b4cb66a
Ahmadpu <ahmad1.ngxoft@gmail.com>
2021-05-17 21:01:28 +0500

    add order place api

--- routes/orders.js
+++ routes/orders.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const Product = require("../models/product");
 const checkAuth = require("../middlewares/checkAuth");
 const orderController = require("../controllers/orders");
+const cart = require("../models/cart");
 
 router.post("/", orderController.orders_post);
 
@@ -15,6 +16,25 @@ router.get(
 
 router.get("/", orderController.orders_getall);
 // router.post('/',checkAuth,orderController.orders_post);
+router.post('/order-placed',async(req,res)=>{
+  const token1 = req.headers.authorization.split(" ")[1];
+  const decoded  =jwt.verify(token1, process.env.JWT_KEY);
+  const userId=decoded.userId
+  Cart.find({cart:userId}).exec().then(result=>{
+    var array=result.products;
+    for(var i=0;i<=(array.length);i++){
+      var total = array[i].price+total
+      return total;
+    }
+    res.status(200).json({
+      message:"Order has been placed",
+      'payment method':req.body.paymentMethod,
+      "shipping Method":req.body.shippingMethod,
+      "Payment Status":req.body.paymentStatus
+    })
+  }
+  )
+})
 router.get("/:orderId", orderController.orders_getId);
 router.delete("/:orderid", orderController.order_delete);
 router.delete("/:orderid", orderController.order_update);
